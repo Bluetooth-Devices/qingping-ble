84497a622e1812efc5ecba7605a49a879b9ec3e6
J. Nick Koston <nick@koston.org>
2022-08-10 10:00:50 -1000

    Update src/qingping_ble/__init__.py

--- src/qingping_ble/__init__.py
+++ src/qingping_ble/__init__.py
@@ -13,7 +13,7 @@ from sensor_state_data import (
 
 from .parser import QingpingBluetoothDeviceData
 
-__version__ = "0.5.1"
+__version__ = "0.0.1"
 
 __all__ = [
     "QingpingBluetoothDeviceData",
