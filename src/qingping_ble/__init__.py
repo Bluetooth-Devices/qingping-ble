57cf26bc9f860a3a2f15278a95d83f95c4d7061d
J. Nick Koston <nick@koston.org>
2022-08-10 10:00:20 -1000

    feat: init repo

--- src/qingping_ble/__init__.py
+++ src/qingping_ble/__init__.py
@@ -1,1 +1,28 @@
-__version__ = "0.0.1"
+"""Parser for Qingping BLE advertisements."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import QingpingBluetoothDeviceData
+
+__version__ = "0.5.1"
+
+__all__ = [
+    "QingpingBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
