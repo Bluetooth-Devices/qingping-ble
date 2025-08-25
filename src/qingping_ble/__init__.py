332d85c5c57446ee30cb7d9abdbe330045a00cdf
semantic-release <semantic-release>
2025-08-25 22:53:59 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- src/qingping_ble/__init__.py
+++ src/qingping_ble/__init__.py
@@ -16,7 +16,7 @@ from sensor_state_data import (
 
 from .parser import QingpingBluetoothDeviceData
 
-__version__ = "0.10.0"
+__version__ = "1.0.0"
 
 __all__ = [
     "QingpingBluetoothDeviceData",
